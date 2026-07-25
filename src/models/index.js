59b91ad6e8ecd9d95f97ce0c0abae29bf995491c
Vũ Chí Bảo K18 HCM <Baovcse182733@fpt.edu.vn>
2026-07-25 13:56:06 +0700

    feat(seed): enrich seed-demo.js with complete datasets for Admin, Manager, Staff, and User roles

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@ const OtpVerification = require("./user/OtpVerification");
 
 const { Building, BuildingManager, Floor, Gate, ParkingSlot, VehicleType, Zone } = require("./building");
 const { PricePolicy, ReservationPolicy, LongTermPackage, LongTermSubscription, ViolationType } = require("./policy");
-const { ParkingSession, Shift, StaffShift } = require("./operations");
+const { ParkingSession, Shift, StaffShift, Feedback } = require("./operations");
 const { Payment, WalletTransaction, BuildingWallet, BuildingWalletTransaction } = require("./finance");
 const { AuditLog, Incident, Notification } = require('./log');
 module.exports = {
@@ -27,6 +27,7 @@ module.exports = {
   Incident,
   Notification,
   ParkingSession,
+  Feedback,
   Payment,
   WalletTransaction,
   BuildingWallet,
